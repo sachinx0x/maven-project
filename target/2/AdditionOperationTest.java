10ce5264c1dee0f7cc95219bec165b6b7d60587d
sachinx0x <151711560+sachinx0x@users.noreply.github.com>
2025-05-26 11:40:26 +0530

    c

---
+++ target/2/AdditionOperationTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AdditionOperationTest {
+
+    @Test
+    public void testAddition() {
+        double num1 = 5;
+        double num2 = 10;
+        double expectedSum = num1 + num2;
+
+        double actualSum = num1 + num2;
+
+        assertEquals(expectedSum, actualSum, 0.01);
+    }
+}
